0f158eccc06173bc380277f1574f9c1741fdabcb
naoya <t_naoya_k@yahoo.co.jp>
2019-07-02 16:45:38 +0900

    Create_school_select

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,23 @@
 class EventsController < ApplicationController
   def new
     @event = Event.new
+    @school = School.new
   end
 
   def create
-    Event.create(event_params)
+    school = School.create(school_params) unless school_params[:name] == ""
+    event = Event.new(event_params)
+    event.school_id = school.id unless school_params[:name] == ""
+    event.save
   end
 
   private
 
   def event_params
-    params.require(:event).permit(:name)
+    params.require(:event).permit(:name, :school_id)
+  end
+
+  def school_params
+    params.require(:school).permit(:name)
   end
 end
--- db/migrate/20190702013255_remove_table_to_customer.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTableToCustomer < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :customers
-  end
-end
